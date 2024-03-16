e4be4278c23b60a823f6ca9270ae983274a07883
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-17 01:04:19 +0500

    update

---
+++ 47 Advanced Array Destructuring/app.ts
@@ -0,0 +1,8 @@
+let Laptops=[{make:"HP",model:"Spectre X360",year:2021},
+             {make:"Dell",model:"X16 R1",year:2024},
+             {make:"Apple",model:"MacBook Pro",year:2020}];
+let [Laptop1,Laptop2,Laptop3] =Laptops;
+console.log(Laptop1);
+console.log(Laptop2);
+console.log(Laptop3);
+            
