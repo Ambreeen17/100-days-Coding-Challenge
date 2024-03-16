e268adcb885d842f7ee3facdad79b6c5478b03ca
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-17 01:05:22 +0500

    update

---
+++ 47 Advanced Array Destructuring/app.js
@@ -0,0 +1,7 @@
+var Laptops = [{ make: "HP", model: "Spectre X360", year: 2021 },
+    { make: "Dell", model: "X16 R1", year: 2024 },
+    { make: "Apple", model: "MacBook Pro", year: 2020 }];
+var Laptop1 = Laptops[0], Laptop2 = Laptops[1], Laptop3 = Laptops[2];
+console.log(Laptop1);
+console.log(Laptop2);
+console.log(Laptop3);
