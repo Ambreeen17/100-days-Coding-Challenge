49427f9fa292026e9a5c73d3eebafaf43b28f192
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-18 07:49:00 +0500

    update

---
+++ 50 Multiline Templete Literals/app.ts
@@ -0,0 +1,6 @@
+ myIdealDay=`My ideal day would involve:
+1.Reciting the Holy Quran with translation .
+2.Doing meditation .
+3.Ending the day by reading a good book.`
+//logging yhe multilines string to he console''
+console.log(myIdealDay);
