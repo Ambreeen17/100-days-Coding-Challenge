8d1c7888ef7484cf44a25bbafa98d60b53d1dfae
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-19 07:48:14 +0500

    update

---
+++ 52 Make a Smartphone Object/app.ts
@@ -0,0 +1,20 @@
+//Sets up details about a smartphone
+let smartphone={
+    make:"Oppo",
+    model:"F21 Pro 5G",
+    specs:{
+        Storage:"128GB - 8GB RAM",
+        screenSize:"6.43 inches	",
+        screenResolution:"1080 x 2400 pixel",
+        batteryLife:"25 hours",
+        processor:"Qualcomm SM6375 Snapdragon 695 5G (6 nm)	",
+        internalMemory:"128 GB	"	,
+        Ram:"128GB",
+        OperatingSystem	:"Android 12 OS	"
+    }
+};
+//show what we have setup about yhe smartphone
+console.log(smartphone);
+
+
+
