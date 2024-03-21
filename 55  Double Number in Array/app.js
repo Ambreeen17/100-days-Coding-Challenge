a01df3db0444d8c17b39d29a1bbe6288e7b796c3
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-21 23:09:02 +0500

    update

---
+++ 55  Double Number in Array/app.js
@@ -0,0 +1,6 @@
+//Starts with a list of numbers
+var oddNumbers = [1, 3, 5, 7, 9, 11];
+//Doubles the number
+var doubleNumbers = oddNumbers.map(function (oddNumber) { return oddNumber * 2; });
+//show the new list of doubled numbers 
+//console.log(doubleNumbers);
