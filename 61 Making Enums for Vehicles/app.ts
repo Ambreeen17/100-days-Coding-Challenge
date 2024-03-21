46eac1a0b541a28c377bca6948246a3283a29301
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-22 02:59:27 +0500

    update

---
+++ 61 Making Enums for Vehicles/app.ts
@@ -0,0 +1,6 @@
+//Making a list(enum) for different types of vehicles
+enum VehiclesType {Aerplane,Train,Car,Truck,Bus,Motorcycle,Cycle}
+//showing one type of vehicles from the list
+console.log(VehiclesType.Train);
+console.log(VehiclesType.Aerplane);
+console.log(VehicleType.Cycle);
