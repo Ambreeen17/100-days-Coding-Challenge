cdf9f9191a4ceabef33e24754d4ada82edd1f9be
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-21 23:07:27 +0500

    update

---
+++ 55  Double Number in Array/app.ts
@@ -0,0 +1,6 @@
+//Starts with a list of numbers
+let oddNumbers=[1,3,5,7,9,11];
+//Doubles the number
+let doubleNumbers=oddNumbers.map(oddNumber=>oddNumber*2);
+//show the new list of doubled numbers 
+console.log(doubleNumbers);
