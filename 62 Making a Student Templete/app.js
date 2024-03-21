455edf2cc4c0306a94c488b061c39435bb2461bb
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-22 03:45:08 +0500

    update

---
+++ 62 Making a Student Templete/app.js
@@ -0,0 +1,9 @@
+//Filling the blueprint with an example student
+var Student = {
+    Name: "Zaharah",
+    Age: 18,
+    Courses: ["Chemisry", "Physics", "Statistics"]
+};
+//Showing the student's information
+console.log(Student);
+
