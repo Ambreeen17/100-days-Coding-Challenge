ae1a5e22744a4e7aeb1b22f75c04fe6d57b47b1f
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-22 06:49:15 +0500

    update

---
+++ 63 Shape Shifter/app.js
@@ -0,0 +1,16 @@
+//Showing the student's information
+console.log(Student);
+//Describing a rectangle using our shape type
+var circle = {
+    kind: "circle",
+    radius: 7,
+};
+//Describing a rectangle using our shape type
+var rectangle = {
+    kind: "rectangle",
+    width: 55,
+    height: 77,
+};
+//showing what we decribed
+console.log(circle);
+console.log(rectangle);
