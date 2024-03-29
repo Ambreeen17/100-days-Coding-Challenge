9b7144057af18803a68ace827306ab3e8c54da35
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-30 01:53:18 +0500

    update

---
+++ 83 Converting to Upper and Lower Case/app.ts
@@ -0,0 +1,7 @@
+function convertCase(str:string ){
+    let upperStr=str.toUpperCase();
+    let loweStr=str.toLowerCase();
+    console.log("Uppercase:",upperStr,"Lower case:",loweStr);
+}
+//example
+convertCase("Pakisan Zindabad");
