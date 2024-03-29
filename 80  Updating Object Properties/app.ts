a540eae3630cb28b22d6cda8e684566d947fb08c
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-30 01:39:21 +0500

    update

---
+++ 80  Updating Object Properties/app.ts
@@ -0,0 +1,12 @@
+//starting with myNewCar object
+let myCar ={
+    make:"Honda ",
+    model:"Civic",
+    year:2023,
+};
+//Adding a new property'color'and updating 'year'
+
+myCar.color="Black";//add a new property "color"
+myCar.year=2024;//updates the year property
+//showing the update car  objects
+console.log(myCar);
