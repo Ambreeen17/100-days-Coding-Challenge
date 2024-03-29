ac02a4dbdda86c8df1fcc8f608945dc3ce9197fd
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-29 22:44:44 +0500

    update

---
+++ 72 Block Scope With let and const/app.ts
@@ -0,0 +1,17 @@
+//Demonstrating block scope
+block scope{
+let blockLet="visible inside the block";
+const blockConst ="also only inside the block";
+console.log(blockLet);//works fine here
+console.log(blockConst);//also fine here
+}
+try{console.log(blockLet);//this will fail
+}catch (error){
+    console.log("blockLet is not accessible outside the block.");  
+}
+try {
+    console.log(blockConst);//this will also fail''
+}catch(error){
+    ("blockConst is not accessible outside the block");
+}
+//this show that le and const keep variables safe inside the block where they are defined
