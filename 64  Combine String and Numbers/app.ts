89792d2f2eb947a7170a6156cbf1bf4da53b2a0e
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-29 22:09:41 +0500

    update

---
+++ 64  Combine String and Numbers/app.ts
@@ -0,0 +1,13 @@
+// This function mixes a text and a  number into one text function
+function combineStringAndNumber(text:string,number:number):
+string{
+    //join the text and number into a single text
+    return text+number;
+}
+console.log(combineStringAndNumber("Age:",20));//Age:20
+ function combineString_Number(text1:string,number:number,text2:string):
+ string{
+    return text1+number+text2
+ }
+ console.log(combineString_Number("Probook",6560,"b"));//Probook6560b
+
