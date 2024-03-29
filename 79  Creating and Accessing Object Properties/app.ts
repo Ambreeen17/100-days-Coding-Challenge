90287abf410e1b6b1523594e186e133060fe876a
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-30 01:36:23 +0500

    update

---
+++ 79  Creating and Accessing Object Properties/app.ts
@@ -0,0 +1,8 @@
+//This sets up an object for a car with specific details
+//let myNewCar ={
+    make:"Honda ",
+    model:"Civic",
+    year:2023,
+};
+//Accessing and showing the car's model
+console.log(myNewCar.model);//output Civic'
