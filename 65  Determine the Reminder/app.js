748692350459c8e032017c059d287064504a6839
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-29 22:14:45 +0500

    update

---
+++ 65  Determine the Reminder/app.js
@@ -0,0 +1,7 @@
+ //This function finds the leftover od dividing two numbers
+function remainder(num1, num2) {
+    //Gives back the leftover of num1 divide num2
+    return num1 % num2;
+}
+console.log(remainder(77, 5)); //remainder =2
+console.log(remainder(70, 5)); //remainder=0
