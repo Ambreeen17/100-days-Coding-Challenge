73a37e33de8baf2d64fc47334cd8adb2fb54643d
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-29 22:27:36 +0500

    update

---
+++ 67 Arithmetic with Mixed Types/app.js
@@ -0,0 +1,5 @@
+function addNumberAndString(number1, numberString) {
+    return number1 + Number(numberString);
+}
+console.log(addNumberAndString(10, "5"));
+console.log(addNumberAndString(100, "99"));
