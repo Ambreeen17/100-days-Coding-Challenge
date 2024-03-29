f110c9a73077b2dcb8fbd1c2e89312135e279860
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-30 01:22:54 +0500

    update

---
+++ 76  Function Parameters and Return Values/app.js
@@ -0,0 +1,8 @@
+//This function adds two numbers and returns the result
+function addNumbers(num1, num2) {
+    //calculates the sum of num1 and num2
+    return num1 + num2;
+}
+//Calling he function with two numbers and logging the result
+console.log(addNumbers(99, 90));
+//Here we ask our function to add 99 and 90 and it tells us the answer
