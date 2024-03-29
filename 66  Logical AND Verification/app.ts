dc044bd8423c70b95a9717d9e4c38890875a8b0a
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-29 22:22:51 +0500

    update

---
+++ 66  Logical AND Verification/app.ts
@@ -0,0 +1,16 @@
+//This function sees if both inputs are true
+  function checkBothTrue(val1:boolean,val2:boolean):boolean{
+    return val1&&val2;
+  }
+console.log(checkBothTrue(true,false));//show false
+console.log(checkBothTrue(true,true));//show true
+console.log(checkBothTrue(1>8,2>8));//show true
+console.log(checkBothTrue(8>6,8>9));//show true
+
+
+
+
+
+
+  
+
