580bbd21bd03b1e7083661e35881816f946b5618
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-30 01:42:24 +0500

    update

---
+++ 80  Updating Object Properties/app.js
@@ -0,0 +1,10 @@
+var myCar = {
+    make: "Honda ",
+    model: "Civic",
+    year: 2023,
+};
+//Adding a new property'color'and updating 'year'
+myCar.color = "Black"; //add a new property "color"
+myCar.year = 2024; //updates the year property
+//showing the update car  objects
+console.log(myCar);
